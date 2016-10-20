1985fe2b2fa3706c0f45c28abd2866419dd7e379
Chaz-Mykl <chaz.mykl@gmail.com>
2016-10-19 18:19:57 -0600

    We have AN input working.

--- function.js
+++ function.js
@@ -1,3 +1,10 @@
 var title = $('.siteTitle');
 var url = $('.siteURL');
 var submit = $('.submit');
+var bookmarkTitle = $('.cardTitle');
+var bookmarkURL = $('.cardURL');
+
+$('.submit').on('click', function() {
+  var newLink = title.val();
+  bookmarkTitle.text(newLink);
+});
