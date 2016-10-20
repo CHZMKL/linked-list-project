71d4dd3357af68aa949db7c41fad1600c83f0bd8
Chaz-Mykl <chaz.mykl@gmail.com>
2016-10-19 23:30:23 -0600

    Functionality. Appends properly, now trying to make the read button do it's thing.

--- function.js
+++ function.js
@@ -2,23 +2,31 @@ var title = $('.siteTitle');
 var url = $('.siteURL');
 var submit = $('.submit');
 var rightSide = $('.containerRight');
-var bookmarkTitle = $('.cardTitle');
-var bookmarkURL = $('.cardURL');
 
-// $('.submit').on('click', function() {
-//   var newLink = title.val();
-//   bookmarkTitle.text(newLink);
-// });
-//
-// $('.submit').on('click', function() {
-//   var newURL = url.val();
-//   bookmarkURL.text(newURL);
-// });
 
-$('.submit').on('click', function() {
+submit.on('click', function() {
+  var bookmarkTitle = $('.cardTitle');
+  var bookmarkURL = $('.cardURL');
   var newLink = title.val();
-  bookmarkTitle.text(newLink);
   var newURL = url.val();
+  bookmarkTitle.text(newLink);
+  title.val("");
   bookmarkURL.text(newURL);
-  rightSide.append('<li class="defaultCard"><p class="cardTitle">' + newLink + '</p><hr size="1"><p class="cardURL">' + newURL + '</p><hr size="1"><button class="read">Read</button><button class="delete">Delete</button></li>');
+  url.val("");
+  rightSide.prepend('<li class="defaultCard"><p class="cardTitle">' + newLink + '</p><hr size="1"><p class="cardURL">' + newURL + '</p><hr size="1"><button class="read">Read</button><button class="delete">Delete</button></li>');
 });
+
+
+
+
+// function submitButtonToggle() {
+//   if (title.val() && url.val()) {
+//     submit.prop('disabled', false);
+//   } else {
+//     submit.prop('disabled', true);
+//   }
+// }
+//
+// title.on('keyup', function() {
+//   console.log(submitButtonToggle());
+// });
