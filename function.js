d5e9eb4ea1566a63024917e41cd44eeb221fc98a
Chaz-Mykl <chaz.mykl@gmail.com>
2016-10-19 20:51:45 -0600

    Able to append new cards to the right panel. However, they're going to the right and not down.

--- function.js
+++ function.js
@@ -1,15 +1,24 @@
 var title = $('.siteTitle');
 var url = $('.siteURL');
 var submit = $('.submit');
+var rightSide = $('.containerRight');
 var bookmarkTitle = $('.cardTitle');
 var bookmarkURL = $('.cardURL');
+
+// $('.submit').on('click', function() {
+//   var newLink = title.val();
+//   bookmarkTitle.text(newLink);
+// });
+//
+// $('.submit').on('click', function() {
+//   var newURL = url.val();
+//   bookmarkURL.text(newURL);
+// });
 
 $('.submit').on('click', function() {
   var newLink = title.val();
   bookmarkTitle.text(newLink);
-});
-
-$('.submit').on('click', function() {
   var newURL = url.val();
   bookmarkURL.text(newURL);
+  rightSide.append('<li class="defaultCard"><p class="cardTitle">' + newLink + '</p><hr size="1"><p class="cardURL">' + newURL + '</p><hr size="1"><button class="read">Read</button><button class="delete">Delete</button></li>');
 });
